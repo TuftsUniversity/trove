1fe991bbec393a94dcaa4a539b2484014d9de8e9
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-04-09 10:16:46 -0400

    Added our trove stuff to the search filters.

--- app/models/search_builder.rb
+++ app/models/search_builder.rb
@@ -1,10 +1,24 @@
-# frozen_string_literal: true
-class SearchBuilder < Blacklight::SearchBuilder
+class SearchBuilder < Hyrax::CatalogSearchBuilder
   include Blacklight::Solr::SearchBuilderBehavior
-  # Add a filter query to restrict the search to documents the current user has access to
-  include Hydra::AccessControlsEnforcement
-  include Hyrax::SearchFilters
+  include BlacklightRangeLimit::RangeLimitBuilder
 
+  # Add a filter query to restrict the search to documents the current user has access to
+  #include Hydra::AccessControlsEnforcement
+  #include Hyrax::SearchFilters
+
+  self.default_processor_chain += [:add_trove_filter, :suppress_embargo_records]
+
+  def add_trove_filter(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << 'displays_in_sim:trove'
+  end
+
+  # Override default behavior so admin users can see unpublished works in the search results
+  def suppress_embargo_records(solr_parameters)
+    solr_parameters[:fq] ||= []
+    solr_parameters[:fq] << '-suppressed_bsi:true'
+    solr_parameters[:fq] << '-embargo_release_date_dtsi:[NOW TO *]'
+  end
 
   ##
   # @example Adding a new step to the processor chain
