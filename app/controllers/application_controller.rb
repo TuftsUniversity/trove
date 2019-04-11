92e1753d203f4e943f34b5d699170a759e21743e
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-04-11 14:50:04 -0400

    Required authentication across site

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,9 @@ class ApplicationController < ActionController::Base
   rescue_from DeviseLdapAuthenticatable::LdapException do |exception|
     render :text => exception, :status => 500
   end
+
+  before_action :authenticate_user!
+
   helper Openseadragon::OpenseadragonHelper
   # Adds a few additional behaviors into the application controller
   include Blacklight::Controller
