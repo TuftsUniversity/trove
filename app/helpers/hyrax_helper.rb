fb29f915cfc53666218f348d0102726e62756641
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-09-13 10:13:35 -0400

    Created a sort_works helper to sort the documents in the collection view and added it to the list and gallery views.

--- app/helpers/hyrax_helper.rb
+++ app/helpers/hyrax_helper.rb
@@ -2,4 +2,46 @@ module HyraxHelper
   include ::BlacklightHelper
   include Hyrax::BlacklightOverride
   include Hyrax::HyraxHelperBehavior
+
+  def sort_works(documents)
+    order = @collection.work_order
+
+    if validate_matching_orders(order, documents)
+      ordered_docs = []
+
+      order.each do |id|
+        documents.each do |doc|
+          if doc.id == id
+            ordered_docs << doc
+            break
+          end
+        end
+      end
+
+      ordered_docs
+    else
+      documents
+    end
+  end
+
+  def validate_matching_orders(order, documents)
+    return false if order.nil?
+
+    if order.count != documents.count
+      byebug
+      logger.error("ERROR: CollectionOrder count does not match actual work count in collection.")
+      return false
+    end
+
+    document_ids = documents.map { |d| d.id }
+    if order.sort != document_ids.sort
+      byebug
+      logger.error("ERROR: CollectionOrder ids don't match actual work ids in collection.")
+      logger.error("Order: #{order}")
+      logger.error("Documents: #{document_ids}")
+      return false
+    end
+
+    true
+  end
 end
