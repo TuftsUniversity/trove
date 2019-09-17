633b4bc03241e5573460ae10e3f7d8898cc96305
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-09-17 13:32:18 -0400

    Added chromedriver for js testing and database cleaner

--- Gemfile
+++ Gemfile
@@ -17,8 +17,7 @@ gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
 gem 'uglifier', '>= 1.3.0'
 # See https://github.com/rails/execjs#readme for more supported runtimes
-gem 'therubyracer', platforms: :ruby
-
+gem 'therubyracer', platforms: :ruby 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
 # Turbolinks makes navigating your web application faster. Read more: https://github.com/turbolinks/turbolinks
@@ -70,11 +69,12 @@ group :development, :test do
   gem 'rspec-rails'
   gem 'factory_bot_rails'
   gem 'ffaker'
+  gem 'database_cleaner'
 
   gem 'capybara', '~> 2.13'
   gem 'selenium-webdriver'
   gem 'launchy'
-  #gem 'webdrivers', '~> 4.0'
+  gem 'webdrivers', '~> 4.0'
 
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,13 @@ abort("The Rails environment is running in production mode!") if Rails.env.produ
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+# Adding chromedriver for js testing.
+Capybara.server = :webrick
+Capybara.register_driver(:chrome) do |app|
+  Capybara::Selenium::Driver.new(app, browser: :chrome)
+end
+Capybara.javascript_driver = :chrome
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
@@ -30,6 +37,7 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
+
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
@@ -58,4 +66,12 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.after do
+    DatabaseCleaner.clean
+  end
 end
