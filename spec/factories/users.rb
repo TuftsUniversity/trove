1d8841bc583f9a3233e50a3908a03dbc07a8777f
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-09-17 13:54:09 -0400

    User specs are working. Added factory_bot methods to rails_helper.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,11 +9,11 @@ FactoryBot.define do
       "#{FFaker::Internet.user_name}#{n}"
     end
     password { 'password' }
-    #display_name { FFaker::Name.name }
-    #after(:create) { |user| user.remove_role(:admin) }
+    display_name { FFaker::Name.name }
+    after(:create) { |user| user.remove_role(:admin) }
 
     factory :admin do
-      after(:create) { |user| user.add_role(:admin) }
+      after(:create) { |user| user.add_role('admin') }
     end
   end
 end
