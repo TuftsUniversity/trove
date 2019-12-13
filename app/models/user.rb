692161c4285ee588ce87301de2585d5f1fa48b27
Mike Korcynski <mkorcy@gmail.com>
2019-12-13 16:42:18 -0500

    adds token support

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   # Connects this user object to Hydra behaviors.
   include Hydra::User
   # Connects this user object to Role-management behaviors.
