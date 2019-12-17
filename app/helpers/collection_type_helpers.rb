d1e65ec5b81f9fd7c506ed906219f96c0c66c6c3
Travis Lilleberg <travis.lilleberg@tufts.edu>
2019-12-17 13:51:20 -0500

    Added the Create Collection buttonson the side bar

--- app/helpers/collection_type_helpers.rb
+++ app/helpers/collection_type_helpers.rb
@@ -13,6 +13,21 @@ module CollectionTypeHelpers
     @course_gid ||= Hyrax::CollectionType.where(title: "Course Collection").first.gid
   end
 
+  ##
+  # @function
+  # Gets the Personal Collection id from the db.
+  def personal_id
+    @personal_id ||= Hyrax::CollectionType.where(title: "Personal Collection").first.id
+  end
+
+  ##
+  # @function
+  # Gets the Course Collection id from the db.
+  def course_id
+    @course_id ||= Hyrax::CollectionType.where(title: "Course Collection").first.id
+  end
+
+
   def is_course_collection?(collection)
     collection.collection_type.title == "Course Collection"
   end
