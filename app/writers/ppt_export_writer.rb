93d923a17672c14d2a6267eaf333d58134a9e6fc
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-01-07 13:18:18 -0500

    Sometimes work_orders can be nil, and ppt writer needs to allow for that

--- app/writers/ppt_export_writer.rb
+++ app/writers/ppt_export_writer.rb
@@ -64,8 +64,11 @@ class PptExportWriter
 
     def images_with_paths
 
-      work_order = JSON.parse(collection.work_order)
-      work_order = collection.member_object_ids if work_order.empty?
+      if collection.work_order.nil? || collection.work_order.empty?
+        work_order = collection.member_object_ids
+      else
+        work_order = JSON.parse(collection.work_order)
+      end
       work_order.flatten.map { |member|
         member_image = Image.find(member)
         file_set = member_image.file_sets[0]
