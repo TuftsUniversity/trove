1325affe0f1f88c3055eb71d6323ef12dca792e7
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-01-08 11:24:16 -0500

    Fixed bug where works weren't being copied with their collections. Reorganized TuftsCollectionControllerBehavior a bit.

--- app/controllers/concerns/tufts_collection_controller_behavior.rb
+++ app/controllers/concerns/tufts_collection_controller_behavior.rb
@@ -5,19 +5,17 @@ module TuftsCollectionControllerBehavior
   include CollectionTypeHelpers
 
   ##
-  # Copies collections, along with their child/parent collections, works, and work order.
+  # Copies collections, along with their works, and work order.
+  # Copies are always private collections that belong to the copier.
   def copy
-    new_collection = ::Collection.new(@collection.attributes.except('id', 'collection_type_gid', 'depositor', 'legacy_pid'))
-    new_collection.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
-    new_collection.collection_type_gid = personal_gid
-    new_collection.apply_depositor_metadata(current_user.user_key)
-    new_collection.save
+    @new_collection = create_copy
     ActiveFedora::SolrService.instance.conn.commit
 
-    Hyrax::Collections::PermissionsCreateService.create_default(collection: new_collection, creating_user: current_user)
-    new_collection.update_order(@collection.work_order, :work) unless @collection.work_order.empty?
-
+    copy_works if @collection.member_objects.present?
+    set_permissions
+    copy_work_order if @collection.work_order.present?
     ActiveFedora::SolrService.instance.conn.commit
+
     redirect_to root_path, notice: t('hyrax.dashboard.my.action.collection_create_success')
   end
 
@@ -46,4 +44,41 @@ module TuftsCollectionControllerBehavior
       end
     end
   end
+
+  private
+
+    ##
+    # Creates a copy of @collection. Transfers metadata. Sets defaults. Commits to Solr so we can move forward.
+    def create_copy
+      new_collection = ::Collection.new(@collection.attributes.except('id', 'collection_type_gid', 'depositor', 'legacy_pid'))
+      new_collection.visibility = Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+      new_collection.collection_type_gid = personal_gid
+      new_collection.apply_depositor_metadata(current_user.user_key)
+      new_collection.save
+      new_collection
+    end
+
+    ##
+    # Copies works from @collection to new_collection.
+    # @param {Collection} new_collection
+    #   The collection to copy the works into.
+    def copy_works
+      work_ids = []
+      @collection.member_objects.each do |m|
+        work_ids << m.id unless m.collection?
+      end
+      @new_collection.add_member_objects(work_ids) unless work_ids.empty?
+    end
+
+    ##
+    # Sets permissions on @new_collection
+    def set_permissions
+      Hyrax::Collections::PermissionsCreateService.create_default(collection: @new_collection, creating_user: current_user)
+    end
+
+    ##
+    # Copies the work order from @collection to @new_collection.
+    def copy_work_order
+      @new_collection.update_order(@collection.work_order, :work)
+    end
 end
