27300b5c2487197650c8c4bd73b609660a916e42
Mike Korcynski <mkorcy@gmail.com>
2020-01-08 15:37:30 -0500

    change over to LB

--- config/environments/stage.rb
+++ config/environments/stage.rb
@@ -1,9 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.port = '80'
-  config.host = 'tdil-dev-02.lib.tufts.edu'
-
+  config.port = '443'
+  config.host = 'trove-dev.tufts.edu'
+  config.protocol = 'https'
+  config.log_level = :debug
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
