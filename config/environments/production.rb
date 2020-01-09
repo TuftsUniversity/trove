dbd409792eb2ab349a7faf85597bfc92ed9d0a68
Mike Korcynski <mkorcy@gmail.com>
2020-01-09 17:07:30 -0500

    mail configruation -- TODO: finalize recipiant for prod after testing

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,17 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "trove_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
+  # Mail Settings
+  # Ignore bad email addresses and do not raise email delivery errors.
+  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { host: ENV['RAILS_HOST'] }
+  config.action_mailer.perform_caching = false
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.tufts.edu",
+    port: 25
+  }
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
