eaf6b220b58d9cc778501c4eb73b6b1571bd3762
Mike Korcynski <mkorcy@gmail.com>
2020-01-10 14:25:38 -0500

    pare down dashboard for admin role

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,15 +1,16 @@
 class Ability
   include Hydra::Ability
-  
-  include Hyrax::Ability
-  self.ability_logic += [:everyone_can_create_curation_concerns]
 
+  include Hyrax::Ability
+  self.ability_logic += [:everyone_can_create_curation_concerns, :trove_admin_permissions] 
+  self.ability_logic -= [:admin_permissions]
   # Add this to your ability_logic if you want all logged in users to be able
   # to submit content
   def everyone_can_create_curation_concerns
     return unless registered_user?
     can :create, [Collection]
   end
+  
   # Define any customized permissions here.
   def custom_permissions
     # Limits deleting objects to a the admin user
@@ -26,5 +27,20 @@ class Ability
     can [:advanced], Image
     can [:dl_powerpoint], Collection
     can [:dl_pdf], Collection
+    
   end
+  def trove_admin_permissions
+    return unless admin?
+    # TODO: deprecate this. We no longer have a dashboard just for admins
+    can :read, :admin_dashboard
+    alias_action :edit, to: :update
+    alias_action :show, to: :read
+    alias_action :discover, to: :read
+    can :update, :appearance
+    can :manage, String # The identifier of a work or FileSet
+    can :manage, curation_concerns_models
+    
+    can :manage, :collection_types
+  end
+
 end
