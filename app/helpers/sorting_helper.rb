9db80c181cf7cd0ce186c8710c991647ef48dd07
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-01-14 10:21:35 -0500

    Sidebar collections now obeying the manual sorting parameters. There is no user interface to set such parameters yet, though

--- app/helpers/sorting_helper.rb
+++ app/helpers/sorting_helper.rb
@@ -3,15 +3,19 @@ module SortingHelper
   # Gets the work or subcollection order for the current collection.
   # @param (:sym) type
   #   :work or :subcollection.
-  def get_collection_order(type)
-    if @collection.nil?
-      if @presenter.nil?
-        logger.error("ERROR: Couldn't find collection or presenter.")
-        return []
+  def get_collection_order(type, collection_id = nil)
+    if(collection_id.nil? || !Collection.exists?(collection_id))
+      if @collection.nil?
+        if @presenter.nil? || !@presenter.respond_to?(:id)
+          Rails.logger.error("\nERROR: Couldn't find collection or presenter.\n")
+          return []
+        end
+        collection = Collection.find(@presenter.id)
+      else
+        collection = @collection
       end
-      collection = Collection.find(@presenter.id)
     else
-      collection = @collection
+      collection = Collection.find(collection_id)
     end
 
     case type
@@ -30,18 +34,21 @@ module SortingHelper
   # Sorts subcollections for display
   # @param {arr} colls
   #   The collections from solr to sort.
-  def sort_subcollections(colls)
-    order = get_collection_order(:subcollection)
+  def sort_subcollections(colls, parent_id = nil)
+    order = get_collection_order(:subcollection, parent_id)
     return colls if order.blank?
-    colls.sort_by { |e| order.index(e.id) || 1000 }
+    colls.sort_by { |e| order.index(e[:id]) || 1000 }
   end
 
   ##
   # Sorts the course collections at the top-level in the sidebar
+  # @param {arr} colls
+  #   Array of {title: '', id: ''} records
+  # @param {int|nil} user_id
+  #   The user_id if loading personal collections
   def sort_top_level_collections(colls, user_id = nil)
     order = user_id.nil? ? TopLevelCollectionOrder.course_collection_order : TopLevelCollectionOrder.search_by_user(user_id)
-    return colls if order.empty?
-
+    return colls if order.blank?
     colls.sort_by { |e| order.index(e[:id]) || 1000 }
   end
 end
