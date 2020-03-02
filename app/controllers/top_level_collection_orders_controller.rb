827a020c9f26871e24996cd7c4d063eee149a1c7
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-03-02 11:11:43 -0500

    Gotta remove cache after updating sidebar

--- app/controllers/top_level_collection_orders_controller.rb
+++ app/controllers/top_level_collection_orders_controller.rb
@@ -4,11 +4,13 @@ class TopLevelCollectionOrdersController < ApplicationController
     return if params[:order].blank?
     order = JSON.parse(params[:order])
     TopLevelCollectionOrder.set_course_collection_order(order)
+    Rails.cache.delete 'views/collections-sidebar-courses'
   end
 
   def set_personal_order
     return if params[:order].blank? || params[:id].blank?
     order = JSON.parse(params[:order])
     TopLevelCollectionOrder.set_for_user(params[:id], order)
+    Rails.cache.delete 'views/collections-sidebar-courses'
   end
 end
