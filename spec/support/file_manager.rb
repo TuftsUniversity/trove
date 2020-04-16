0d02cd3205ce1e289e2af0eeaef3aacb528569ec
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-04-16 18:00:39 -0400

    Reconfigured tests to work with altered ExportManagerService

--- spec/support/file_manager.rb
+++ spec/support/file_manager.rb
@@ -2,8 +2,8 @@ require 'fileutils'
 
 module FileManager
   def create_export_dirs
-    FileUtils.mkdir_p(Tufts::ExportManagerService.pdf_path)
-    FileUtils.mkdir_p(Tufts::ExportManagerService.ppt_path)
+    FileUtils.mkdir_p(Tufts::ExportManagerService.export_base_path + '/pdfs')
+    FileUtils.mkdir_p(Tufts::ExportManagerService.export_base_path + '/ppts')
   end
 
   def destroy_export_dirs
