36d8e23e0f523bf0399d812a6890f2838b477dd3
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-04-16 14:47:45 -0400

    Starting to get controllers to work with new Service

--- app/controllers/concerns/tufts_collection_controller_behavior.rb
+++ app/controllers/concerns/tufts_collection_controller_behavior.rb
@@ -29,22 +29,28 @@ module TuftsCollectionControllerBehavior
     @curated_collection = ::Collection.find(params[:id])
     respond_to do |format|
       format.pdf do
-        exporter = PdfCollectionExporter.new(@curated_collection)
-        send_file(exporter.export, filename: exporter.pdf_file_name, type: "application/pdf", :disposition => 'attachment')
+        export_manager = Tufts::ExportManagerService.new(@curated_collection, 'pdf')
+        send_file(export_manager.retrieve_asset,
+                  filename: export_manager.readable_filename,
+                  type: "application/pdf",
+                  disposition: 'attachment'
+        )
       end
     end
   end
 
   ##
-  # Generates a Powerpoint of all the images in the collection for user to donwload.
+  # Generates a Powerpoint of all the images in the collection for user to download.
   def dl_powerpoint
     @curated_collection = ::Collection.find(params[:id])
     respond_to do |format|
       format.pptx do
-        exporter = PowerPointCollectionExporter.new(@curated_collection)
-        send_file(exporter.export,
-                  filename: exporter.pptx_file_name,
-                  type: "application/vnd.openxmlformats-officedocument.presentationml.presentation", :disposition => 'attachment')
+        export_manager = Tufts::ExportManagerService.new(@curated_collection, 'pptx')
+        send_file(export_manager.retrieve_asset,
+                  filename: export_manager.readable_filename,
+                  type: "application/vnd.openxmlformats-officedocument.presentationml.presentation",
+                  disposition: 'attachment'
+        )
       end
     end
   end
