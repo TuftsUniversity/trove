3f25521bf4ade0bf59d11e88aa793fd34e845b99
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-04-30 07:50:50 -0400

    Added image factory from MIRA

---
+++ spec/factories/image.rb
@@ -0,0 +1,11 @@
+FactoryBot.define do
+  factory :image do
+    title { ["Image: #{FFaker::Movie.title}"] }
+    creator { ["Image Creator"] }
+    visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+
+    transient do
+      user { nil }
+    end
+  end
+end
