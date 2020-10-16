4f113348ae38cd61c9fc823565ebb388c8f252c8
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-10-16 16:11:11 -0400

    Added custom matchers to search the CollectionsSidebar less verbosely

--- spec/support/custom_matchers.rb
+++ spec/support/custom_matchers.rb
@@ -7,3 +7,25 @@ RSpec::Matchers.define :exist_on_filesystem do
   failure_message { |actual| "Expected #{actual} to exist as a directory or file." }
   failure_message_when_negated { |actual| "Expected #{actual} to not exist as a directory or file." }
 end
+
+# Easy way to search the Collections sidebar for a title
+RSpec::Matchers.define :show_in_personal_sidebar do
+  match do |actual|
+    visit '/'
+    expect(find('#personal_collections')).to have_content(actual.title.first)
+  end
+
+  failure_message { |actual| "Expected #{actual} to be listed in Personal Collections Sidebar" }
+  failure_message_when_negated { |actual| "Expected #{actual} to not be listed in Personal Collections Sidebar" }
+end
+
+# Easy way to search the Collections sidebar for a title
+RSpec::Matchers.define :show_in_course_sidebar do
+  match do |actual|
+    visit '/'
+    expect(find('#course_collections')).to have_content(actual)
+  end
+
+  failure_message { |actual| "Expected #{actual} to be listed in Course Collections Sidebar" }
+  failure_message_when_negated { |actual| "Expected #{actual} to not be listed in Course Collections Sidebar" }
+end
