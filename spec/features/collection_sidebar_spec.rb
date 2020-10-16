abf3d4e87ed6cbd4293db6f98ad94433d9da756a
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-10-16 16:12:24 -0400

    Added more tests regarding the visibility of various types of collections in the CollectionsSidebar, and the collapsing/uncollapsing of child collections in that sidebar

--- spec/features/collection_sidebar_spec.rb
+++ spec/features/collection_sidebar_spec.rb
@@ -1,61 +1,75 @@
 require 'rails_helper'
+require 'webdrivers/chromedriver'
 include FeatureMacros
 i_need_ldap
 
 RSpec.feature 'Collection Sidebar' do
-  context 'personal collections' do
-    let(:user) { create(:ldap_user) }
-    let(:good_coll) { create(:personal_collection, user: user) }
-    let(:coll_by_other_user) { create(:personal_collection) }
-    let(:child_coll) { create(:personal_collection, user: user, parent: good_coll) }
+  let(:user) { create(:ldap_user) }
+  let(:pers_coll) { create(:personal_collection, user: user) }
 
-    before(:each) do
-      sign_in(user)
+  before(:each) do
+    sign_in(user)
+    visit '/'
+  end
+
+  context 'to display or not to display' do
+    scenario 'valid collections should display', slow: true do
+      pers_coll
+      crs_coll = create(:course_collection)
+      expect(pers_coll).to show_in_personal_sidebar
+      expect(crs_coll).to show_in_course_sidebar
+    end
+
+    scenario 'PrivateCollections by a different user should not display', slow: true do
+      coll = create(:personal_collection)
+      expect(coll).not_to show_in_personal_sidebar
+    end
+
+    scenario 'restricted CourseCollections should not display', slow: true do
+      coll = create(
+        :course_collection,
+        visibility: Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PRIVATE
+      )
+      expect(coll).not_to show_in_course_sidebar
+    end
+
+    scenario 'non-trove collections should not display', slow: true do
+      coll = create(:personal_collection, user: user, displays_in: nil)
+      expect(coll).not_to show_in_personal_sidebar
+    end
+
+    scenario 'child collection should be hidden, until revealed', js: true, slow: true do
+      pers_coll
+      child_coll = create(:personal_collection, user: user, parent: pers_coll)
       visit '/'
+      expect(page).to have_selector("##{child_coll.id}", visible: false)
+      find("li[data-id=\"#{pers_coll.id}\"] > button").click
+      expect(page).to have_selector("##{child_coll.id}")
+    end
+  end
+
+  context 'adding a collection via + button' do
+    scenario 'non-admin should only see + button on personal collections', slow: true do
+      expect(find('#personal_collections')).to have_content('+')
+      expect(find('#course_collections')).not_to have_content('+')
     end
 
-    context 'to display or not to display' do
-      scenario 'valid collection should display' do
-        good_coll
-        title = good_coll.title.first
-        visit '/'
-        expect(find('#personal_collections')).to have_content(title)
-      end
+    scenario 'collection appears in sidebar', slow: true do
+      user.add_role('admin')
+      visit('/')
 
-      scenario 'collection by a different user should not display' do
-        coll_by_other_user
-        title = coll_by_other_user.title.first
-        visit '/'
-        expect(find('#personal_collections')).not_to have_content(title)
-      end
+      ['course', 'personal'].each do |type|
+        title = "I am showing, #{type}"
+        div = "##{type}_collections"
+        within(div) { click_link('+') }
 
-      skip 'child collection should be hidden, until revealed' do
+        expect(current_path).to eq('/dashboard/collections/new')
+        fill_in('collection_title', with: title)
+        click_button('Save')
+
+        expect(current_path).to eq('/')
+        expect(find(div)).to have_content(title)
       end
     end
-
-    # context 'adding a collection via + button' do
-    #   scenario 'non-admin should only see + button on personal collections' do
-    #     expect(find('#personal_collections')).to have_content('+')
-    #     expect(find('#course_collections')).not_to have_content('+')
-    #   end
-    #
-    #   scenario 'collection appears in sidebar' do
-    #     user.add_role('admin')
-    #     visit('/')
-    #
-    #     ['course', 'personal'].each do |type|
-    #       title = "I am showing, #{type}"
-    #       div = "##{type}_collections"
-    #       within(div) { click_link('+') }
-    #
-    #       expect(current_path).to eq('/dashboard/collections/new')
-    #       fill_in('collection_title', with: title)
-    #       click_button('Save')
-    #
-    #       expect(current_path).to eq('/')
-    #       expect(find(div)).to have_content(title)
-    #     end
-    #   end
-    # end
   end
 end
