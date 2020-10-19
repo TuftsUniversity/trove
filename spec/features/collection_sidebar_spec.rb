9298ea0dd96d953c9e8c07101057167aa9cac142
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-10-19 15:22:04 -0400

    Added tests for rearranging collections in the Sidebar - finishing the CollectionSidebar tests

--- spec/features/collection_sidebar_spec.rb
+++ spec/features/collection_sidebar_spec.rb
@@ -9,7 +9,6 @@ RSpec.feature 'Collection Sidebar' do
 
   before(:each) do
     sign_in(user)
-    visit '/'
   end
 
   context 'to display or not to display' do
@@ -46,14 +45,17 @@ RSpec.feature 'Collection Sidebar' do
       find("li[data-id=\"#{pers_coll.id}\"] > button").click
       expect(page).to have_selector("##{child_coll.id}")
     end
+
+    scenario 'non-admin should only see + and rearrange buttons on personal collections' do
+      visit '/'
+      expect(find('#personal_collections')).to have_content('+')
+      expect(page).to have_css('button.reorder-personal')
+      expect(find('#course_collections')).not_to have_content('+')
+      expect(page).not_to have_css('button.reorder-course')
+    end
   end
 
   context 'adding a collection via + button' do
-    scenario 'non-admin should only see + button on personal collections', slow: true do
-      expect(find('#personal_collections')).to have_content('+')
-      expect(find('#course_collections')).not_to have_content('+')
-    end
-
     scenario 'collection appears in sidebar', slow: true do
       user.add_role('admin')
       visit('/')
@@ -70,6 +72,70 @@ RSpec.feature 'Collection Sidebar' do
         expect(current_path).to eq('/')
         expect(find(div)).to have_content(title)
       end
+    end
+  end
+
+  context 'rearranging TopLevelCollections' do
+    ##
+    # Returns Personal Collection sidebar NodeList and ids.
+    def get_sidebar_els_and_ids
+      list = all('.top-level-personal-collections > li')
+      return list, list.map { |node| node['data-id'] }
+    end
+
+    ##
+    # Swaps the Personal Collection sidebar collections around a bit.
+    def rearrange_collections(arr_btn, collections)
+      arr_btn.click
+      collections[0].drag_to(collections[2])
+      arr_btn.click
+      sleep 1
+    end
+
+    scenario 'Clicking the rearrange button enables/disables rearranging', js: true, slow: true do
+      pers_coll
+      visit('/')
+      arr_btn = find('button.reorder-personal')
+
+      expect(page).not_to have_css('.ui-sortable-handle')
+      arr_btn.click
+      expect(page).to have_css('.ui-sortable-handle')
+      arr_btn.click
+      expect(page).not_to have_css('.ui-sortable-handle')
+      expect(1).to eq 1
+    end
+
+    ##
+    # This tests a lot of things, because I don't want to fully instantiate three collections
+    #    over and over. It tests:
+    # * Rearranging the sidebar generates a TopLevelCollectionOrder if it doesn't exist.
+    # * Rearranging the sidebar updates the TopLevelCollectionOrder if it does exist.
+    # * The TopLevelCollectionOrder matches the order of the ids in the HTML.
+    scenario 'Rearranging saves the order as a TopLevelCollectionOrder', js: true, slow: true do
+      pers_coll
+      create(:personal_collection, user: user)
+      create(:personal_collection, user: user)
+      visit('/')
+      collections_to_drag, _ = get_sidebar_els_and_ids
+      arr_btn = find('button.reorder-personal')
+
+      expect(TopLevelCollectionOrder.count).to eq(0)
+
+      # Rearranging the sidebar generates a TopLevelCollectionOrder if it doesn't exist.
+      rearrange_collections(arr_btn, collections_to_drag)
+      expect(TopLevelCollectionOrder.count).to eq(1)
+
+      # The TopLevelCollectionOrder matches the order of the ids in the HTML.
+      collections_to_drag, ids = get_sidebar_els_and_ids
+      expect(TopLevelCollectionOrder.search_by_user(user.id)).to eq(ids)
+
+      # Rearranging the sidebar updates the TopLevelCollectionOrder if it does exist.
+      rearrange_collections(arr_btn, collections_to_drag)
+      expect(TopLevelCollectionOrder.count).to eq(1)
+
+      # The TopLevelCollectionOrder matches the order of the ids in the HTML.
+      _, ids = get_sidebar_els_and_ids
+      expect(TopLevelCollectionOrder.search_by_user(user.id)).to eq(ids)
     end
   end
 end
