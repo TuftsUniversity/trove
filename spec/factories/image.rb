8618ade7995e4b05c585f69cc8e376d3c914b166
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-10-23 10:12:44 -0400

    Fixed issue where tried to access Image.title, which doesn't exist

--- spec/factories/image.rb
+++ spec/factories/image.rb
@@ -1,8 +1,9 @@
 FactoryBot.define do
   factory :image do
-    title { ["Image: #{FFaker::Image.title}"] }
+    sequence(:title) { |n| ["Image: #{n}"] }
     creator { ["Image Creator"] }
     visibility { Hydra::AccessControls::AccessRight::VISIBILITY_TEXT_VALUE_PUBLIC }
+    displays_in { ['trove'] }
 
     transient do
       user { nil }
