e4692fb0274a918e27f2609d3786422202d7b753
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-11-18 11:07:13 -0500

    Tested all layout changes on collection show pages. Fixed small bug on collection show pages. Added patching comment to one view

--- spec/features/layout_changes_spec.rb
+++ spec/features/layout_changes_spec.rb
@@ -9,22 +9,55 @@ RSpec.feature 'Layout Changes' do
     sign_in(user)
   end
 
-  scenario 'should be no hidden dropdown menu in the main search bar' do
+  scenario 'no dropdown menu in the main search bar' do
     visit '/'
     expect(find('#search-form-header')).not_to have_css('button.dropdown-toggle')
   end
 
-  scenario 'should not be a citations option on Images' do
+  scenario 'no citations option on Images' do
     image = create(:image)
     visit "/concern/images/#{image.id}"
     expect(page).not_to have_css('.citations button')
   end
 
-  scenario 'should not be a bookmark checkbox option on images in collections' do
+  scenario 'lots of things shouldnt display on collection show pages' do
     coll = create(:personal_collection, user: user)
     image = create(:image)
     coll.add_member_objects([image.id])
     visit "/collections/#{coll.id}"
+
+    # Header shouldn't contain:
+    # * Visibility badge
+    # * Total item count
+    # * Last updated
+    # * Creator
+    header = find('header.hyc-generic')
+    expect(header).not_to have_content('Public')
+    expect(header).not_to have_content('Private')
+    expect(header).not_to have_content('1 Item')
+    expect(header).not_to have_content('Last Updated:')
+    expect(header).not_to have_content('Created by:')
+
+    # No Collection Info box
+    expect(page).not_to have_css('.hyc-metadata')
+
+    # Search results shouldn't contain sorting options
+    expect(find('.hyc-bl-sort')).not_to have_content('Sort by')
+    # Per page options and default per page selection should match CatalogController config
+    per_page_opts = all('#per_page > option').map(&:value)
+    expect(per_page_opts).to eq(["12","24","50","100"])
+    expect(find('#per_page > option[selected]').text).to eq("24")
+
+    # no bookmark checkbox on images
     expect(find("#document_#{image.id}")).not_to have_css('.bookmark_toggle')
+
+    # Search results list view shouldn't contain:
+    # * Visibility column or links
+    # * Is part of: collections info
+    click_on('List')
+    results = find('.hyc-bl-results')
+    expect(results).not_to have_content('Visibility')
+    expect(results).not_to have_css('.visibility-link')
+    expect(results).not_to have_content('Is part of:')
   end
 end
