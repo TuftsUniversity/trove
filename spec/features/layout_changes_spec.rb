ce7b58175bf11af5f57ad3bb8c3bab855bfe2187
Travis Lilleberg <travis.lilleberg@tufts.edu>
2020-11-20 10:34:08 -0500

    Moved collection page layout changes out of general layout changes tests because they were so massive

--- spec/features/layout_changes_spec.rb
+++ spec/features/layout_changes_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 include FeatureMacros
 i_need_ldap
 
-RSpec.feature 'Layout Changes' do
+RSpec.feature 'General Layout Changes' do
   let(:user) { create(:ldap_user) }
 
   before(:each) do
@@ -20,44 +20,8 @@ RSpec.feature 'Layout Changes' do
     expect(page).not_to have_css('.citations button')
   end
 
-  scenario 'lots of things shouldnt display on collection show pages' do
-    coll = create(:personal_collection, user: user)
-    image = create(:image)
-    coll.add_member_objects([image.id])
-    visit "/collections/#{coll.id}"
-
-    # Header shouldn't contain:
-    # * Visibility badge
-    # * Total item count
-    # * Last updated
-    # * Creator
-    header = find('header.hyc-generic')
-    expect(header).not_to have_content('Public')
-    expect(header).not_to have_content('Private')
-    expect(header).not_to have_content('1 Item')
-    expect(header).not_to have_content('Last Updated:')
-    expect(header).not_to have_content('Created by:')
-
-    # No Collection Info box
-    expect(page).not_to have_css('.hyc-metadata')
-
-    # Search results shouldn't contain sorting options
-    expect(find('.hyc-bl-sort')).not_to have_content('Sort by')
-    # Per page options and default per page selection should match CatalogController config
-    per_page_opts = all('#per_page > option').map(&:value)
-    expect(per_page_opts).to eq(["12","24","50","100"])
-    expect(find('#per_page > option[selected]').text).to eq("24")
-
-    # no bookmark checkbox on images
-    expect(find("#document_#{image.id}")).not_to have_css('.bookmark_toggle')
-
-    # Search results list view shouldn't contain:
-    # * Visibility column or links
-    # * Is part of: collections info
-    click_on('List')
-    results = find('.hyc-bl-results')
-    expect(results).not_to have_content('Visibility')
-    expect(results).not_to have_css('.visibility-link')
-    expect(results).not_to have_content('Is part of:')
+  scenario 'no Issue Type in contact form' do
+    visit 'contact'
+    expect(page).not_to have_content('Issue Type')
   end
 end
